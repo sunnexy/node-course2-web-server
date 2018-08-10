5f1194f8e0b668fe559972475b80b1592d009d32
Paschal <sogbonnapaschal@gmail.com>
2018-08-10 02:02:16 -0700

    Initial commit

---
+++ server.js
@@ -0,0 +1,54 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+var app = express();
+
+hbs.registerPartials(__dirname + '/views/partials')
+app.set('view engine', 'hbs'); 
+
+
+app.use((req, res, next) => {
+	var now = new Date().toString();
+	var log = `${now}: ${req.method} ${req.url}`;
+	console.log(log);
+	fs.appendFile('server.log', log + '\n', (err) => {
+		if (err) {
+			console.log('Unable to append to server.log.')
+		}
+	});
+	next();
+});
+
+// app.use((req, res, next) => {
+// 	res.render('error.hbs')
+// });
+app.use(express.static(__dirname + '/public'));
+hbs.registerHelper('getCurrentYear', () => {
+	return new Date().getFullYear()
+});
+hbs.registerHelper('screenIt', (text) => {
+	return text.toUpperCase();
+});
+
+app.get('/', (req, res) => {
+	res.render('home.hbs', {
+		pageTitle: 'Home Page',
+		pageInfo: 'Welcome to my website'
+
+	})
+});//to set up a handler for http get request(like a route in laravel)
+
+app.get('/about', (req, res) => {
+	res.render('about.hbs', {
+		pageTitle: 'About Page'
+	});
+});
+app.get('/bad', (req, res) => {
+	res.send({
+		errorPage: 'Unable to send resquest'
+	});
+});
+app.listen(3000, () => {
+	console.log('Server is up now');
+});
