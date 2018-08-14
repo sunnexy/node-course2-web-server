cabee03b4cf7d2c0e51cebc9745cba739f89bdb6
Paschal <sogbonnapaschal@gmail.com>
2018-08-14 00:32:45 -0700

    Setup start script and heroku port

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const hbs = require('hbs');
 const fs = require('fs');
 
+const port = process.env.PORT || 3000;
 var app = express();
 
 hbs.registerPartials(__dirname + '/views/partials')
@@ -49,6 +50,6 @@ app.get('/bad', (req, res) => {
 		errorPage: 'Unable to send resquest'
 	});
 });
-app.listen(3000, () => {
-	console.log('Server is up now');
+app.listen(port, () => {
+	console.log(`Server is running on ${port}`);
 });
